b6bfa7fe01a258b846a511cf1eddc94c6a81e54c
Dong Duong <dhdong1510@gmail.com>
2021-05-31 00:53:08 +0700

    Implement Blogs entities on micro-service icwblog

---
+++ icwblog/src/main/java/com/icw/blog/repository/BlogRepository.java
@@ -0,0 +1,14 @@
+package com.icw.blog.repository;
+
+import com.icw.blog.domain.Blog;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Blog entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+}
